87d5898e9c2004cc42d2ea778ffd9781136fee92
tuckerr10 <muhfrendy90@gmail.com>
2026-04-24 00:25:47 +0700

    refactor: integrasi splash screen di main
    Mengubah property home pada MaterialApp untuk memuat SplashScreen terlebih dahulu sebelum menjalankan AuthGuard.

--- lib/main.dart
+++ lib/main.dart
@@ -3,23 +3,37 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'firebase_options.dart';
 import 'features/auth/presentation/providers/auth_provider.dart';
+import 'core/routes/app_router.dart';
+import 'core/routes/auth_guard.dart';
+import 'features/dashboard/presentation/providers/product_provider.dart';
+
+// KITA TAMBAHKAN: Jangan lupa import file splash_screen kamu.
+import 'features/splash/presentation/screens/splash_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  
-  // Inisialisasi Firebase [cite: 175]
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
-  runApp(
-    MultiProvider(
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (_) => AuthProvider()),
+        ChangeNotifierProvider(create: (_) => ProductProvider()),
       ],
-      child: const MaterialApp(
-        home: Scaffold(body: Center(child: Text("Bag Store Ready!"))),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Bag Store',
+        routes: AppRouter.routes,
+        // KITA UBAH: Jadikan SplashScreen sebagai halaman pertama yang dimuat
+        home: const SplashScreen(),
       ),
-    ),
-  );
+    );
+  }
 }
